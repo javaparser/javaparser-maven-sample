0880e51117e2c8f4ce146a283009139c036b2db8
Danny van Bruggen <55701+matozoid@users.noreply.github.com>
2020-04-09 10:59:40 +0200

    Upgrade dependency and create some output

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -7,16 +7,20 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 import com.github.javaparser.utils.CodeGenerationUtils;
+import com.github.javaparser.utils.Log;
 import com.github.javaparser.utils.SourceRoot;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 
 /**
  * Some code that uses JavaParser.
  */
 public class LogicPositivizer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        // JavaParser has a minimal logging class that normally logs nothing.
+        // Let's ask it to write to standard out:
+        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
+        
         // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
         // In this case the root directory is found by taking the root from the current Maven module,
         // with src/main/resources appended.
@@ -25,6 +29,8 @@ public class LogicPositivizer {
         // Our sample is in the root of this directory, so no package name.
         CompilationUnit cu = sourceRoot.parse("", "Blabla.java");
 
+        Log.info("Positivizing!");
+        
         cu.accept(new ModifierVisitor<Void>() {
             /**
              * For every if-statement, see if it has a comparison using "!=".

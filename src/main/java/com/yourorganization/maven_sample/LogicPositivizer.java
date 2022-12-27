4bced1c85dcf26aaa0ee99420659e994804ec138
namgonkim <32921225+namgonkim@users.noreply.github.com>
2022-12-27 16:37:29 +0900

    [feat] change whole project to cu list

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -1,65 +1,67 @@
 package com.yourorganization.maven_sample;
 
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
-import com.github.javaparser.utils.CodeGenerationUtils;
-import com.github.javaparser.utils.Log;
-import com.github.javaparser.utils.SourceRoot;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import com.github.javaparser.symbolsolver.utils.SymbolSolverCollectionStrategy;
+import com.github.javaparser.utils.*;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Some code that uses JavaParser.
  */
 public class LogicPositivizer {
-    public static void main(String[] args) {
-        // JavaParser has a minimal logging class that normally logs nothing.
-        // Let's ask it to write to standard out:
-        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
-        
-        // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
-        // In this case the root directory is found by taking the root from the current Maven module,
-        // with src/main/resources appended.
-        SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot(LogicPositivizer.class).resolve("src/main/resources"));
+    public static void main(String[] args) throws IOException {
+        // 파싱할 프로젝트 루트 경로
+        Path root = Paths.get(("/Users/namgonkim/workspace/java-baseball"));
 
-        // Our sample is in the root of this directory, so no package name.
-        CompilationUnit cu = sourceRoot.parse("", "Blabla.java");
+        // 파싱 전략 설정
+        // 여기서 어케하면 jar에 있는 심볼이나 디렉토리 내에 있는 심볼을 매핑시킬 수 있다는데 좀더 조사가 필요함.
+        // https://github.com/javaparser/javasymbolsolver
+        SymbolSolverCollectionStrategy symbolSolverCollectionStrategy = new SymbolSolverCollectionStrategy();
+        ProjectRoot projectRoot = symbolSolverCollectionStrategy.collect(root);
+        // 전체 프로젝트 ast로 조회
+        List<SourceRoot> sourceRootList = projectRoot.getSourceRoots();
 
-        Log.info("Positivizing!");
-        
-        cu.accept(new ModifierVisitor<Void>() {
-            /**
-             * For every if-statement, see if it has a comparison using "!=".
-             * Change it to "==" and switch the "then" and "else" statements around.
-             */
-            @Override
-            public Visitable visit(IfStmt n, Void arg) {
-                // Figure out what to get and what to cast simply by looking at the AST in a debugger! 
-                n.getCondition().ifBinaryExpr(binaryExpr -> {
-                    if (binaryExpr.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
-                        /* It's a good idea to clone nodes that you move around.
-                            JavaParser (or you) might get confused about who their parent is!
-                        */
-                        Statement thenStmt = n.getThenStmt().clone();
-                        Statement elseStmt = n.getElseStmt().get().clone();
-                        n.setThenStmt(elseStmt);
-                        n.setElseStmt(thenStmt);
-                        binaryExpr.setOperator(BinaryExpr.Operator.EQUALS);
-                    }
-                });
-                return super.visit(n, arg);
+        for(SourceRoot sourceRoot : sourceRootList) {
+            List<ParseResult<CompilationUnit>> parseResults = sourceRoot.tryToParse();
+            for(ParseResult<CompilationUnit> parseResult : parseResults) {
+                Optional<CompilationUnit> optionalCompilationUnit = parseResult.getResult();
+                if(optionalCompilationUnit.isPresent()) {
+                    CompilationUnit cu = optionalCompilationUnit.get();
+
+                    // cu를 활용
+
+                }
             }
-        }, null);
 
-        // This saves all the files we just read to an output directory.  
-        sourceRoot.saveAll(
-                // The path of the Maven module/project which contains the LogicPositivizer class.
-                CodeGenerationUtils.mavenModuleRoot(LogicPositivizer.class)
-                        // appended with a path to "output"
-                        .resolve(Paths.get("output")));
+            // 소스코드를 저장
+            // This saves all the files we just read to an output directory.
+            sourceRoot.saveAll(
+                    // The path of the Maven module/project which contains the LogicPositivizer class.
+                    CodeGenerationUtils.mavenModuleRoot(LogicPositivizer.class)
+                            // appended with a path to "output"
+                            .resolve(Paths.get("output")));
+        }
+
     }
 }

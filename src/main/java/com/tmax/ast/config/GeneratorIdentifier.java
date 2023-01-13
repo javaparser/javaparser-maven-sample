82a3243c9c2faf65b57ef54fe8bbe9df04a2d372
mincheol_lee <mincheol_lee@tmax.co.kr>
2023-01-13 10:46:28 +0900

    method call expr add

--- src/main/java/com/tmax/ast/config/GeneratorIdentifier.java
+++ src/main/java/com/tmax/ast/config/GeneratorIdentifier.java
@@ -8,5 +8,7 @@ public class GeneratorIdentifier {
     public static Long variableDeclarationId = 1L;
     public static Long methodDeclarationId = 1L;
     public static Long parameterId = 1L;
+    public static Long argumentId = 1L;
     public static Long returnMapperId = 1L;
+    public static Long methodCallExprId = 1L;
 }
---
+++ src/main/java/com/tmax/ast/dto/MethodCallExprDTO.java
@@ -0,0 +1,57 @@
+package com.tmax.ast.dto;
+
+import com.github.javaparser.ast.Node;
+
+import java.util.List;
+
+public class MethodCallExprDTO {
+    private String name;
+    private Long blockId;
+    private Long methodCallExprId;
+    private Position position;
+    private List<ArgumentDTO> arguments;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getBlockId() {
+        return blockId;
+    }
+    public void setBlockId(Long blockId) {
+        this.blockId = blockId;
+    }
+    public Long getMethodCallExprId() {
+        return methodCallExprId;
+    }
+    public void setMethodCallExprId(Long methodCallExprId) {
+        this.methodCallExprId = methodCallExprId;
+    }
+    public Position getPosition() {
+        return position;
+    }
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public List<ArgumentDTO> getArguments() {
+        return arguments;
+    }
+
+    public void setArguments(List<ArgumentDTO> arguments) {
+        this.arguments = arguments;
+    }
+
+    @Override
+    public String toString() {
+        return "MethodCallExprDTO : {" +
+                "methodCallExprId : " + methodCallExprId +
+                ", blockId : " + blockId +
+                ", name : '" + name + '\'' +
+                ", Position : '" + position +
+                "}\n";
+    }
+
+}
